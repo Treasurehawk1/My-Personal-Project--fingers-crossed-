c06c16c63f37d5734e053467bc1498b6c99b07e9
Treasurehawk1 <steventreasurehawk@gmail.com>
2024-04-15 17:59:54 -0700

    Web Crawler searching for a specific word, not my project (learner)

---
+++ Web Crawler Guided/src/com/netinstructions/crawler/SpiderTest.java
@@ -0,0 +1,11 @@
+package com.netinstructions.crawler;
+
+public class SpiderTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Spider spider = new Spider();
+		spider.search("https://www.youtube.com/", "video");
+	}
+
+}
